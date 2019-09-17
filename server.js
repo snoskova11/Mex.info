7996151abb3a8b521692290b48942c86b8870ef9
snoskova11 <55435193+snoskova11@users.noreply.github.com>
2019-09-17 12:29:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,5 +14,6 @@ const server = http.createServer(function (request, response) {
         response.end(text);
     }
 });
-server.listen(3000);
+console.log('port = ', process.env.PORT);
+server.listen(process.env.PORT || 3000);
 console.log("server started!");
